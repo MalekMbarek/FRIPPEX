6b59a5e3cf73b8152e37cf2aec1ba854ef54df52
Amal-Messaoudi <amalmessaoudi31@gmail.com>
2025-02-11 21:38:47 +0100

    first commit

---
+++ lib/acceuil.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/acceuil.png"),
+            fit: BoxFit.cover, // Couvre tout l'écran
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'acceuil.dart'; // Assure-toi que le nom du fichier est correct
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
